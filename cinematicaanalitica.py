28f3e45fa7cd0da4a633842a6146614957aac6c0
BrennoDom <brennohdomingues@gmail.com>
2025-03-06 16:26:08 -0300

    Merged Files for step2

--- cinematicaanalitica.py
+++ cinematicaanalitica.py
@@ -40,7 +40,6 @@ def rotate_matrix(roll, pitch, yaw, matrix):
 ############ CINEMÁTICA GERAL ############
 def ler_thetas(in_theta1,in_theta2,in_theta3,in_theta4,changes):
 
-    flag = True
     arquivo1 = "./Steps/junta1.tab"
     arquivo2 = "./Steps/junta2.tab"
     arquivo3 = "./Steps/junta3.tab"
@@ -60,10 +59,7 @@ def ler_thetas(in_theta1,in_theta2,in_theta3,in_theta4,changes):
     theta3_degree =  theta3_bruto['MOTION_3.ANG']
     theta4_degree =  theta4_bruto['MOTION_4.ANG']
 
-    # Imprime para verificar
-    #print(theta1_bruto)
-
-    #criando os vetores para armazenar os ângulos corrigidos em radianos
+    # Criando os vetores para armazenar os ângulos corrigidos em radianos
     theta1 = np.zeros(len(theta1_degree))
     theta2 = np.zeros(len(theta1_degree))
     theta3 = np.zeros(len(theta1_degree))
@@ -75,23 +71,25 @@ def ler_thetas(in_theta1,in_theta2,in_theta3,in_theta4,changes):
     theta2_ = in_theta2
     theta3_ = in_theta3
     theta4_ = in_theta4
-    # Corrigindo os ângulos e convertendo para radianos
+
+
     
     for i in range(len(theta1_degree)):
 
         for j in range(len(changes)):
+        # Identifica posição inicial do passo    
             if i== changes[j]:
                 theta1_ = theta1[i-1]
                 theta2_ = theta2[i-1]
                 theta3_ = theta3[i-1]
                 theta4_ = theta4[i-1]
-        
+        # Ajusta a posição atual da junta para a real com a posição inicial do passo
         theta1[i] = theta1_degree[i] + theta1_
         theta2[i] = theta2_degree[i] + theta2_
         theta3[i] = theta3_degree[i] + theta3_
         theta4[i] = theta4_degree[i] + theta4_
+        # Adicionando no array e Convertendo para radianos
         thetas[i] = [m.radians(theta1[i]),m.radians(theta2[i]),m.radians(theta3[i]),m.radians(theta4[i])]
-        #print(theta2[i])
     return thetas
 
 # Função para ler um arquivo CSV contendo os parâmetros de Denavit-Hartenberg DH
@@ -168,43 +166,38 @@ def kinematic(DH_a, DH_alpha, DH_d, DH_theta, M):
     #print(T)
     return T #retorna uma lista com todos as matrizes
 def normalization():
-    aux = 0
-    flag_changes = 0
-    N = StepsPata1.shape[0]
-    print(N)
-    P1_norma = np.zeros(N)
-    changes = []
-    value =0
-    epsilon = 1e-6  # Define um limite para evitar erros numéricos
+    N = StepsPata1.shape[0]  # Número de linhas
+    P1_norma = np.linalg.norm(StepsPata1.iloc[:, 1:3], axis=1)  # Norma Euclidiana (√(X² + Y²))
+    
+    changes = []  # Lista para armazenar os índices das mudanças
+    previous_value = P1_norma[0]  # Valor inicial
+    in_change = False  # Estado de mudança
 
-    # Calcular a norma corretamente com as colunas X e Y
-    for k in range(N-1):
-        P1_norma[k] = np.linalg.norm(StepsPata1.iloc[k, 1:3])  
-        print(P1_norma[k])
-        if P1_norma[k] == P1_norma [k-1]:
-            value = P1_norma[k]
-        if np.isclose(P1_norma[k - 1], value, atol=epsilon) and not np.isclose(P1_norma[k], value, atol=epsilon):
-            
-            changes.append(k-1)
+    for k in range(1, N):
+        if P1_norma[k] != previous_value:  
+            if not in_change:
+                changes.append(k-1)  # Registra início da mudança
+                in_change = True  # Entra no estado de mudança
+        else:
+            if in_change:
+                changes.append(k)  # Registra fim da mudança
+                in_change = False  # Sai do estado de mudança
 
-
-
-
-    # Detectar mudanças 
+        previous_value = P1_norma[k]  # Atualiza valor anterior
 
     print(changes)
     return changes
 # Função principal - main
 if __name__ == '__main__':
-        L = [136.01, 141.42, 152.64]
+        L = [136.01, 141.42, 152.64] #Parâmetros do robô comprimento dos elos
        
 
 
-        DH_a1 = [L[0],L[1],L[2],0]
-        DH_a2 = [L[2],L[1],L[0],0]
+        DH_a1 = [L[0],L[1],L[2],0] #Parâmetros do robô DH
+        DH_a2 = [L[2],L[1],L[0],0] #Parâmetros do robô DH
 
-        DH_alpha = [0,0,0,0]
-        DH_d = [0,0,0,0]
+        DH_alpha = [0,0,0,0] #Parâmetros do robô DH
+        DH_d = [0,0,0,0] #Parâmetros do robô DH
  
 
         STEPSPATA1 = "./Steps/XeYPata1.tab"
@@ -215,56 +208,53 @@ if __name__ == '__main__':
         StepsPata2 = read(STEPSPATA2,1)
         timeread  = StepsPata1["Time      "]
         time_step = len(timeread)
-        changes = normalization()
-        print(changes)
-        thetas = ler_thetas(53.97,-45.84,-66.52,-31.61, changes)
+        changes = normalization() # Identificação dos pontos de troca de cinemática
 
-        flag = True
-        cinematica_x = []
-        cinematica_y = []
-        cinematica_x2 = []
-        cinematica_y2 = []
-        theta1 = []
-        theta2 = []
-        _matrix1 = np.identity(4)
-        _matrix2 = np.identity(4)
-        print(changes)
-        print(len(changes))
-        print(range(len(changes)))
+        thetas = ler_thetas(53.97,-45.84,-66.52,-31.61, changes) # Posições iniciais das juntas e mudanças na base para identificar posição inicial de cada passo
+
+        flag = True # Flag de mudança na base
+        cinematica_x = [] # Resultado da posição X da 1º base
+        cinematica_y = [] # Resultado da posição Y da 1º base
+        cinematica_x2 = [] # Resultado da posição X da 2º base
+        cinematica_y2 = [] # Resultado da posição Y da 2º base
+        theta1 = [] # Thetas da 1º base
+        theta2 = [] # Thetas da 2º base
+        _matrix1 = np.identity(4) # Matriz identidade para início
+        _matrix2 = np.identity(4) # Matriz identidade para início
         for i in range(time_step):
                 
                 theta1.append([thetas[i][0],thetas[i][1],thetas[i][2],thetas[i][3]]) 
                 theta2.append([thetas[i][3],thetas[i][2],thetas[i][1],thetas[i][0]])
-                matrix1 =  kinematic(DH_a1, DH_alpha, DH_d, theta1[i], rotate_matrix(0.0, 0.0, 0.0, _matrix2))
-                matrix2 =  kinematic(DH_a2, DH_alpha, DH_d, theta2[i], rotate_matrix(0.0, 3.14, 0.0, _matrix1))
-                for j in range(len(changes)):
+
+                matrix1 =  kinematic(DH_a1, DH_alpha, DH_d, theta1[i], _matrix2) # Cálculo cinemática para base 1
+                matrix2 =  kinematic(DH_a2, DH_alpha, DH_d, theta2[i], _matrix1) # Cálculo cinemática para base 2
+
+                for j in range(len(changes)): # Identificação de iteração de mudança de base
                     
                     if i== changes[j]:
-                        print(i)
                         if flag:
                             flag = False
                         else:
                             flag = True
 
-                if flag:
-                    #print(timeread[i])
+                if flag: # 1º base
+
                     _matrix1=matrix1[3]
+                    _matrix1=rotate_matrix(0.0, 3.14, 0.0,_matrix1) # Rotação da matriz, para casar na próxima cinemática
                     cinematica_x.append(_matrix1[:3,3][0]) # Posição X do manipulador
                     cinematica_y.append(_matrix1[:3,3][1]+10) # Posição Y ajustada - origem do ADAMs (0,20,0)
                     cinematica_x2.append(_matrix2[:3,3][0])
                     cinematica_y2.append(_matrix2[:3,3][1]+10) 
 
-                else:
-                   # print(i)
+                else: # 2º base
+
                     _matrix2=matrix2[3]
+                    _matrix2=rotate_matrix(0.0, 3.14, 0.0,_matrix2) # Rotação da matriz, para casar na próxima cinemática
                     cinematica_x.append(_matrix1[:3,3][0])
                     cinematica_y.append(_matrix1[:3,3][1]+10) 
                     cinematica_x2.append(_matrix2[:3,3][0]) # Posição X do manipulador
                     cinematica_y2.append(_matrix2[:3,3][1]+10) # Posição Y ajustada - origem do ADAMs (0,20,0)
 
-
-       # cinematica_y[0] = 10.0
-       # cinematica_x[len(cinematica_x)-1] = cinematica_x[len(cinematica_x)-2]
         #PLOT
         # Extraindo as posições X, Y e Z do arquivo do ADAMs
         adams_time = StepsPata1["Time      "]
@@ -288,22 +278,13 @@ if __name__ == '__main__':
         for idx in reset_indices:
             last_time = adams_time.loc[idx - 1]
             adams_time.loc[idx:] += time_offset
-            #print(last_time)
-            #print(adams_time.loc[idx - 1])
-
-            #if idx > 0:  # Ensure idx - 1 is valid
-            #    time_offset += adams_time.loc[idx - 1]
-            #     print(adams_time.loc[idx - 1])
-            #adams_time.loc[idx:] = time_offset
-        
 
         # Store adjusted time back into the DataFrame
         StepsPata1["Adjusted_Time"] = adams_time
 
         # Retrieve the last time step
         last_time_step = adams_time.iloc[-1]
-        
-        #print(cinematica_x2)
+
         # Criando os gráficos para comparar:
         # 1 - Posições convergidas pelo código;
         # 2 - Posições retiradas do Software - ADAMs
